26be000b48479c2815a1f71a267f59f31fe8e2a4
Lahiru Kavinda <lahiruxk@gmail.com>
2025-02-25 16:55:24 +0530

    update

--- components/HeroSection/page.jsx
+++ components/HeroSection/page.jsx
@@ -2,19 +2,20 @@
 import { motion } from "framer-motion";
 import MagnetButton from "../MagnetButton/page";
 import Link from "next/link";
+import WordsMarquee from "../WordsMarquee/page";
 
 export default function HeroSection() {
   return (
     <div className="relative min-h-screen rounded-b-3xl shadow-2xl ring-1 ring-gray-900/10 bg-gray-300 flex items-center justify-center overflow-hidden">
       {/* Background */}
-      <div className="hidden md:block absolute   ">
+      <div className="hidden md:block absolute  bottom-0 right-40 ">
         <motion.img
-          src="/lk.png"
+          src="/lk.webp"
           alt="Lahiru Kavinda"
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
-          className=" sha"
+          className=" w-[730px]"
         />
       </div>
 
@@ -34,7 +35,7 @@ export default function HeroSection() {
         <MagnetButton />
       </Link>
       {/* Text Content */}
-      <div className="absolute right-10 text-center md:text-left text-black max-w-xl">
+      <div className="absolute left-20 text-center md:text-left text-black max-w-xl">
         <motion.h1
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
@@ -52,6 +53,8 @@ export default function HeroSection() {
           Software Engineer & Web Developer
         </motion.p>
       </div>
+
+      <div className="absolute z-20 bottom-10 "><WordsMarquee/></div>
 
       {/* Floating Location */}
       <motion.div
