2ae7ce08447589489b460a9345856fc8a180d574
Lahiru Kavinda <lahiruxk@gmail.com>
2025-02-26 15:58:13 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/StickyFooter/Footer1";
+import Footer2 from "@/components/StickyFooter/Footer2";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +27,8 @@ export default function RootLayout({ children }) {
       >
         {/* <Navbar/> */}
         {children}
+        <Footer/>
+        <Footer2/>
       </body>
     </html>
   );
