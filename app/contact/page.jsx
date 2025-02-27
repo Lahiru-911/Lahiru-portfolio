f8c61cd4a30f618042e8ace7743c9203bd136f23
Lahiru Kavinda <lahiruxk@gmail.com>
2025-02-27 22:09:21 +0530

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,13 @@
-
+import HeroSection from "@/components/HeroSection/page";
 import React from "react";
 
 const Contact = () => {
-  return <div className="text-black">Contact
-
-  </div>;
+  return (
+    <div className="text-black">
+      Contact
+      <HeroSection />
+    </div>
+  );
 };
 
 export default Contact;
