64fa7bc33cebe0e5d2a054a3776eef4fcbe9f30a
Lahiru Kavinda <lahiruxk@gmail.com>
2025-02-28 14:48:08 +0530

    update

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,7 +1,25 @@
+import { BlogCard } from "@/components/BlogCard/BlogCard";
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <BlogCard />
+      <br />
+      <BlogCard />
+      <br />
+      <BlogCard />
+      <br />
+      <BlogCard />
+      <br />
+      <BlogCard />
+      <br />
+    </div>
+  );
 };
 
 export default Blog;
