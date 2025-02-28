cedfacff169bd34909c824990cb1c7a0442bf6f3
Lahiru Kavinda <lahiruxk@gmail.com>
2025-02-28 09:59:09 +0530

    Update Footer.jsx

--- components/StickyFooter/Footer.jsx
+++ components/StickyFooter/Footer.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import Content from './Content';
+import React from "react";
+import Content from "./Content";
 
 export default function Footer() {
   return (
-    <div 
-        className='relative h-[800px]'
-        style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
+    <div
+      className="relative h-[800px]"
+      style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
-        <div className='relative h-[calc(100vh+800px)] -top-[100vh]'>
-            <div className='h-[800px] sticky top-[calc(100vh-800px)]'>
-                <Content />
-            </div>
+      <div className="relative h-[calc(100vh+800px)] -top-[100vh]">
+        <div className="h-[800px] sticky top-[calc(100vh-800px)]">
+          <Content />
         </div>
+      </div>
     </div>
-  )
+  );
 }
-
 // import React from "react";
 // import Content from "./Content";
 
