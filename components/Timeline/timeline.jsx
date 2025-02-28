1b15245eba9a4ca6c99f1d8fceb4b4c0e5e871d2
Lahiru Kavinda <lahiruxk@gmail.com>
2025-02-28 12:06:34 +0530

    Update timeline.jsx

--- components/Timeline/timeline.jsx
+++ components/Timeline/timeline.jsx
@@ -25,7 +25,7 @@ export const Timeline = ({ data }) => {
   return (
     <div className="w-full bg-white font-sans md:px-10" ref={containerRef}>
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
+        <h2 className="text-4xl font-semibold text-black md:text-4xl mb-4 max-w-4xl">
           Changelog from my journey
         </h2>
         <p className="text-neutral-700 text-sm md:text-base max-w-sm">
