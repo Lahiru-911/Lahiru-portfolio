c57ac312ab75c9a07b0f873d85ef00e621f98240
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-03 10:11:34 +0530

    update

--- app/page.js
+++ app/page.js
@@ -46,7 +46,7 @@ const Home = () => {
           {/* <WordsMarquee /> */}
 
           {/* Why Residue Solution */}
-          <div className="grid md:grid-cols-2 m-4 md:m-6 lg:m-7 xl:m-8">
+          <div className="grid md:grid-cols-2 ml-4">
             <div>
               {/* <h1 className="text-[#D8D8D8] text-left font-bold text-4xl sm:text-4xl md:text-6xl lg:text-8xl xl:text-[120px] 2xl:text-[160px]">
                 I'm,
@@ -54,34 +54,34 @@ const Home = () => {
                 Lahiru
                 <br /> Kavinda
               </h1> */}
-              <PerspectiveText/>
+              <PerspectiveText />
             </div>
-            <div className="grid grid-rows-2">
+            <div className="grid grid-rows-2 m-4 md:m-6 lg:m-7 xl:m-8">
               {/* First Tagline */}
               <div className="my-1 md:my-0">
-                <h1 className="font-light w-full md:w-11/12 lg:w-11/12 xl:w-10/12 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
-                  Our primary focus is on business
+                <h1 className="font-light w-full md:w-11/12 lg:w-10/12 xl:w-9/12 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl dm-sans-bold">
+                  Who I Am
                 </h1>
-                <p className="text-justify my-1 md:my-3 lg:my-4 w-5/6 lg:w-10/12 xl:w-3/4 text-[#808080] text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-                  We understand your business, we listen to you. Only then do we
-                  provide you technology recommendations. When we provide our
-                  recommendations, we pride ourselves on being superb
-                  communicators. We speak human! No techy complex jargon here.
+                <p className="text-justify my-1 md:my-3 lg:my-4 w-5/6 lg:w-10/12 xl:w-3/4 text-[#808080] text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl dm-sans-regular">
+                  Aspiring IT professional with a strong foundation in software
+                  development and technology. With experience as a
+                  <span className="font-semibold text-black dm-sans-regular">
+                    {" "}
+                    UX/UI Designer, Software Engineer, and Web Developer,
+                  </span>
+                  I bring a unique blend of technical expertise and creative
+                  problem-solving. Passionate about design, I strive to create
+                  intuitive and visually appealing user experiences.
+                  Detail-oriented and skilled in identifying and documenting
+                  software issues, ensuring optimal performance and user
+                  satisfaction. A proactive collaborator who works closely with
+                  development teams to implement efficient, innovative solutions
+                  while maintaining high-quality software standards.
                 </p>
               </div>
+
               {/* Second Tagline */}
-              <div className="my-1 md:my-3 lg:my-4">
-                <h1 className="font-light text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
-                  We focus on people
-                </h1>
-                <p className="text-justify my-1 md:my-3 lg:my-4 w-5/6 lg:w-10/12 xl:w-3/4 text-[#808080] text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-                  Your people and our own people. Technology fails when you
-                  don’t understand and support the people using it. And once we
-                  have all this in place, our focus is to become your trusted
-                  partner and advisor. We are in this for the long term, and
-                  your success is our success.
-                </p>
-              </div>
+              <div className="my-1 md:my-3 lg:my-4"></div>
             </div>
           </div>
 
