ba9bff21c7ec7ac4a46baf6a1584f78c78e5e9e7
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-03 20:05:17 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 "use client";
+
 import React, { useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import Preloader from "@/components/preloader/Preloader";
 import HeroSection from "@/components/HeroSection/page";
-import WordsMarquee from "@/components/WordsMarquee/page";
 import Textparallax from "@/components/Textparallax/page";
 import SocialMediaIcons from "@/components/GlobeAnimation/SocialMediaIcons";
 import PerspectiveText from "@/components/PerspectiveText/PerspectiveText";
@@ -35,22 +35,23 @@ const Home = () => {
 
   return (
     <div>
-      <AnimatePresence mode="wait">
-        {isLoading && <Preloader />}
-      </AnimatePresence>
+      {/* Preloader */}
+      <AnimatePresence mode="wait">{isLoading && <Preloader />}</AnimatePresence>
 
       {!isLoading && (
         <div>
+          {/* Hero Section */}
           <HeroSection />
 
+          {/* Social Media Icons */}
           <SocialMediaIcons />
-          {/* <WordsMarquee /> */}
 
-          {/* Why Residue Solution */}
+          {/* About Me Section */}
           <div className="grid md:grid-cols-2 ml-4">
             <div>
               <PerspectiveText />
             </div>
+
             <div className="md:pt-10 m-4 md:m-6 lg:m-7 xl:m-8">
               <div className="my-1 md:my-0">
                 <h1 className="w-full md:w-11/12 lg:w-10/12 xl:w-9/12 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl dm-sans-bold font-semibold">
@@ -72,11 +73,10 @@ const Home = () => {
                   development teams to implement efficient, innovative solutions
                   while maintaining high-quality software standards.
                 </p>
-                <a
-                  href="/Lahiru-Resume.pdf"
-                  download="LahiruKavinda-Resume.pdf"
-                >
-                  <button className="cursor-pointer flex items-center justify-between bg-gray-800 px-3 py-2 rounded-full text-white tracking-wider shadow-xl hover:bg-gray-900 hover:scale-105 duration-500 hover:ring-1 font-mono w-[120px] mt-3">
+
+                {/* Resume Download Button */}
+                <a href="/Lahiru-Resume.pdf" download="LahiruKavinda-Resume.pdf">
+                  <button className="cursor-pointer flex items-center justify-between bg-gradient-to-l from-blue-500 to-purple-600 px-3 py-2 rounded-full text-white tracking-wider shadow-xl hover:from-purple-500 hover:to-blue-600 hover:scale-105 duration-500 hover:ring-1 font-mono w-[120px] mt-3">
                     Resume
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -98,31 +98,11 @@ const Home = () => {
             </div>
           </div>
 
+          {/* Text Parallax Section */}
           <Textparallax />
 
-          <a
-            href="/Lahiru-Resume.pdf"
-            download="Lahiru.pdf"
-            className="px-4 py-2 bg-blue-500 rounded-full text-white  hover:bg-blue-600 transition"
-          ></a>
-          <button className="cursor-pointer flex items-center justify-between bg-gray-800 px-3 py-2 rounded-full text-white tracking-wider shadow-xl hover:bg-gray-900 hover:scale-105 duration-500 hover:ring-1 font-mono w-[120px]">
-            Resume
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              className="w-5 h-5 animate-bounce"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
-              />
-            </svg>
-          </button>
-          <div className="mx-5 md:mx-20 lg:mx-24 mb-16 md:mb-24 ">
+          {/* Accordion Section */}
+          <div className="mx-5 md:mx-20 lg:mx-24 mb-16 md:mb-24">
             <Accordion />
           </div>
         </div>
