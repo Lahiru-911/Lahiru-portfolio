41f4ea2b3f68ea0e85085aed5cf4c68888201073
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-05 00:16:39 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
   }, []);
 
   return (
-    <div className="flex justify-center p-5">
-      <div className="rounded-3xl bg-[#706f6f] w-full  h-auto p-6 shadow-lg">
+    <div className="flex justify-center px-5 bg-[#121212]">
+      <div className="rounded-3xl bg-[#121212] w-full  h-auto p-6 shadow-lg">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
           {/* Left Section */}
           <div className="space-y-4">
@@ -20,6 +20,33 @@ const Contact = () => {
             <p className="mb-4 text-xs font-normal sm:text-sm lg:text-base">
               Give us a few details and we’ll offer the best solution.
             </p>
+
+
+
+            <div className="flex h-[34px] text-[14px] text-white/60 w-[240px] items-center bg-[#09090b] border border-white/10 rounded-lg focus-within:ring-2 focus-within:ring-gray-700 focus-within:ring-offset-2 focus-within:ring-offset-[#09090b] transition-all duration-150 ease-in-out">
+              <input
+                placeholder="Email"
+                type="text"
+                name="text"
+                className="bg-transparent text-[#f4f4f5] px-3 py-1 rounded-l-lg focus:outline-none w-full"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffffff"
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="icon icon-tabler icons-tabler-outline icon-tabler-mail mr-3"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
+                <path d="M3 7l9 6l9 -6"></path>
+              </svg>
+            </div>
           </div>
 
           {/* Right Section (Map Box) */}
