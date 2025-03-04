3028e49f31f2cd257f4d7bbb4b1a37d793b6f0c1
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-04 14:31:02 +0530

    Update page.jsx

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,24 +1,23 @@
 import { ProjectParallax } from "@/components/ProjectParallax/projectparallax";
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import Head from "next/head";
 
 const Projects = () => {
   return (
     <div className="bg-[#121212]">
+      <Head>
+        <title>My Projects</title>
+        <meta
+          name="description"
+          content="Check out my latest software development projects."
+        />
+        <meta name="keywords" content="projects, software, React, Next.js" />
+      </Head>
       <ProjectParallax />
-      
+
       <ProjectCard />
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+      <div className="h-10" />
     </div>
   );
 };
