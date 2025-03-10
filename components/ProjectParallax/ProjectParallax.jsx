d41aef7e4b47508e65911e6bcdae7447707c8344
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-10 22:53:18 +0530

    Create ProjectParallax.jsx

---
+++ components/ProjectParallax/ProjectParallax.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React from "react";
+import { ProjectParallaxDemo } from "./projectparallaxdemo";
+
+export function ProjectParallax() {
+  return <ProjectParallaxDemo products={products} />;
+}
+export const products = [
+  {
+    title: "Moonbeam",
+    link: "https://gomoonbeam.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+
+  {
+    title: "Editorially",
+    link: "https://editorially.org",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://app.pixelperfect.quest",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+
+  {
+    title: "Algochurn",
+    link: "https://algochurn.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://ui.aceternity.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "https://tailwindmasterkit.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://smartbridgetech.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://renderwork.studio",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+
+  {
+    title: "Creme Digital",
+    link: "https://cremedigital.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "https://goldenbellsacademy.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "Invoker Labs",
+    link: "https://invoker.lol",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+  {
+    title: "E Free Invoice",
+    link: "https://efreeinvoice.com",
+    thumbnail:
+      "https://res.cloudinary.com/dj3t05pln/image/upload/v1741529237/cmwudfp22nmjvar1xldz.webp",
+  },
+];
