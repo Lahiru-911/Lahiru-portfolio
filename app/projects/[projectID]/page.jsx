3d8c58bfd863e0dc5266b7f19602a62c2d69ee04
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-12 23:43:54 +0530

    Update page.jsx

--- app/projects/[projectID]/page.jsx
+++ app/projects/[projectID]/page.jsx
@@ -51,13 +51,13 @@ const SingleProject = () => {
 
   return (
     <>
-      <div className="bg-[#E0E0E0] max-w-7xl md:mx-auto m-3 md:my-12 px-6 sm:px-8 lg:px-10 py-8 shadow-lg rounded-3xl border border-gray-100">
-        <h1 className="text-left text-4xl sm:text-5xl md:text-6xl font-bold text-black mb-6 tracking-tight">
+      <div className="bg-[#E0E0E0] max-w-7xl lg:mx-auto m-3 md:my-14 px-6 sm:px-8 lg:px-10 py-8 shadow-lg rounded-3xl border border-gray-100">
+        <h1 className="text-left text-3xl lg:text-4xl font-bold text-black mb-5 tracking-tight">
           {project.projectTitle}
         </h1>
 
         {/* Thumbnail */}
-        <div className="relative w-full overflow-hidden rounded-xl bg-gray-200 shadow-md mb-10">
+        <div className="relative w-full overflow-hidden rounded-xl bg-gray-200 shadow-md mb-8">
           <Image
             src={project.projectCoverImage}
             alt="Cover image for project"
@@ -68,25 +68,25 @@ const SingleProject = () => {
         </div>
 
         {/* Project Overview */}
-        <section className="mb-14">
-          <h2 className="text-3xl font-semibold text-gray-900 mb-3">
+        <section className="mb-5 md:mb-10">
+          <h2 className="text-xl md:text-2xl font-semibold text-gray-900 mb-3">
             Project Overview
           </h2>
-          <p className="text-lg text-gray-700 leading-relaxed">
+          <p className="text-base md:text-base text-justify text-gray-700 leading-relaxed">
             {project.detailedProjectDescription}
           </p>
         </section>
 
         {/* Technologies Used */}
-        <section className="mb-14">
-          <h2 className="text-3xl font-semibold text-gray-900 mb-5">
+        <section className="mb-8 md:mb-10">
+          <h2 className="text-xl md:text-2xl font-semibold text-gray-900 mb-5">
             Technologies Used
           </h2>
-          <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-9 gap-4 md:gap-6 text-gray-50">
+          <ul className="grid grid-cols-3 md:grid-cols-6 gap-3 md:gap-6 lg:gap-6 text-xs md:text-sm text-gray-50">
             {project.techStack.map((tech, index) => (
               <li
                 key={index}
-                className=" bg-blue-600 rounded-3xl shadow-sm py-2 px-3 text-center"
+                className=" bg-blue-600 rounded-3xl shadow-sm py-2 px-3 text-center "
               >
                 {tech}
               </li>
@@ -95,11 +95,11 @@ const SingleProject = () => {
         </section>
 
         {/* Screenshots Section */}
-        <section className="mb-16">
-          <h2 className="text-3xl font-semibold text-gray-900 mb-5">
+        <section className="mb-8 md:mb-10">
+          <h2 className="text-xl md:text-2xl font-semibold text-gray-900 mb-5">
             Screenshots
           </h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
             {project.screenshots.map((screenshot, index) => (
               <div
                 key={index}
@@ -118,11 +118,11 @@ const SingleProject = () => {
         </section>
 
         {/* Features */}
-        <section className="mb-12">
-          <h2 className="text-3xl font-semibold text-gray-900 mb-5">
+        <section className="mb-5 md:mb-10">
+          <h2 className="text-xl md:text-2xl font-semibold text-gray-900 mb-5">
             Features
           </h2>
-          <ul className="list-disc pl-6 space-y-2 text-lg text-gray-700">
+          <ul className="list-disc pl-6 space-y-2 text-gray-700 text-base md:text-lg">
             {project.projectFeatures.map((feature, idx) => (
               <li key={idx} className="leading-relaxed">
                 {feature}
@@ -132,14 +132,14 @@ const SingleProject = () => {
         </section>
 
         {/* Links */}
-        <div className="flex flex-col sm:flex-row justify-between md:items-end gap-8 mt-14">
+        <div className="flex flex-col sm:flex-row justify-between md:items-end gap-8 mt-8">
           <div className="flex flex-col gap-6 w-full sm:w-auto">
             {project.deployLink && (
               <a
                 href={project.deployLink}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="px-8 py-4 text-center text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl shadow-lg transition duration-300"
+                className="px-5 md:px-5 py-2 md:py-3 text-center text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl shadow-lg transition duration-300"
               >
                 Visit Live Project
               </a>
@@ -150,7 +150,7 @@ const SingleProject = () => {
                   href={project.repositoryLinks.frontend}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-8 py-4 text-center text-white bg-gray-800 hover:bg-gray-900 rounded-full shadow-lg transition duration-300"
+                  className="px-4 md:px-8 py-2 md:py-4 text-center text-white bg-gray-800 hover:bg-gray-900 rounded-full shadow-lg transition duration-300"
                 >
                   Frontend Code
                 </a>
@@ -161,7 +161,7 @@ const SingleProject = () => {
                   href={project.repositoryLinks.backend}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-8 py-4 text-center text-white bg-gray-800 hover:bg-gray-900 rounded-full shadow-lg transition duration-300"
+                  className="px-4 md:px-8 py-2 md:py-4 text-center text-white bg-gray-800 hover:bg-gray-900 rounded-full shadow-lg transition duration-300"
                 >
                   Backend Code
                 </a>
@@ -171,7 +171,7 @@ const SingleProject = () => {
 
           {/* Go Back Button */}
           <Link href="/projects">
-            <button className="px-6 py-3 border border-red-500 text-gray-800 hover:text-white hover:bg-red-500 rounded-full shadow-md transition duration-300">
+            <button className="px-6 md:px-4 py-2 md:py-3 border border-red-500 text-gray-800 hover:text-white hover:bg-red-500 rounded-full shadow-md transition duration-300">
               Go Back
             </button>
           </Link>
